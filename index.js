40828cc593a1b9b0613fcb13230f5eb43d356aae
atnishat <nishat.mu47@gmail.com>
2022-11-10 16:29:41 +0600

    order added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middle wares
 app.use(cors());
@@ -28,7 +28,7 @@ async function run() {
     try{
 
         const serviceCollection = client.db('service-review').collection('services');
-
+        const orderCollection = client.db('service-review').collection('orders');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -37,9 +37,20 @@ async function run() {
             res.send(services);
         });
 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
 
-
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
 
 
 
