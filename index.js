6806b091cfc8a1c79d901d045bfaed68cb730c48
atnishat <nishat.mu47@gmail.com>
2022-11-10 10:23:14 +0600

    add

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-////////////
-// mmmmmmmmm
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('restaurant web is running')
+})
+
+app.listen(port, () => {
+    console.log(`restaurant web is running on ${port}`);
+})
